fe9a9b9fa48da2e547e2a6b42dd89f61ba90950a
loveCamille <343235900@qq.com>
2025-04-03 16:18:51 +0800

    通用jwt模块

--- src/main/java/com/taylor/jwt/JwtAutoConfiguration.java
+++ src/main/java/com/taylor/jwt/JwtAutoConfiguration.java
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration;
 public class JwtAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean(JwtProperties.class)
+    @ConditionalOnMissingBean(JwtProvider.class)
     public JwtProvider jwtProvider() {
         return new DefaultJwtProvider();
     }
